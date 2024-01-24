a835805f701609352159d5ca35fa5f1f24ee6d85
Chenyu Wang <wangchenyu2017@gmail.com>
2024-01-24 16:56:15 +0900

    feat: prefer type import

--- example/src/imported.ts
+++ example/src/imported.ts
@@ -1,1 +1,7 @@
 export const a = 1;
+
+export type T = 1;
+
+export const Schema = {};
+
+export type Schema = {};
--- example/src/index.ts
+++ example/src/index.ts
@@ -1,4 +1,15 @@
-import { a } from "./imported";
+import { a, T } from "./imported";
+
 import React from 'react';
+import { FC } from 'react';
+import { Schema } from "./imported";
+
+const c: FC;
+
+const f: import('react').FC;
+
+const s: Schema;
+
+const t: T;
 
 console.log(a);
